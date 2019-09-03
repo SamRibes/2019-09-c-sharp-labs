47bdd93a521f0494ede7fb6a680015ee32173452
Samuel Ribeiro <SRibeiro@spartaglobal.com>
2019-09-03 15:44:16 +0100

    Final commit for 03/09

--- labs/lab_07_methods/Program.cs
+++ labs/lab_07_methods/Program.cs
@@ -19,6 +19,11 @@ namespace lab_07_methods
 
             MulitplyNumbers(5);        //gives 500
             MulitplyNumbers(5, 1000);  //gives 5000
+
+            OutParameters(2, 5, out int a);         //a = 2 * 5
+
+            var output = Tuple();
+            Console.WriteLine(output.x, output.y, output.z);
         }
 
         static void DoThisToo()
@@ -30,6 +35,16 @@ namespace lab_07_methods
         static void MulitplyNumbers(int y, int x = 100)
         {
             Console.WriteLine(x*y);
+        }
+
+        static void OutParameters(int x, int y, out int z)
+        {
+            z = x * y;
+        }
+
+        static (int x, string y, bool z) Tuple()
+        {
+            return (2, "Yo", true);
         }
     }
 }

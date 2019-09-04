70e69635c7943c89b9ab5d3ecc6fe91284eb23fb
Samuel Ribeiro <SRibeiro@spartaglobal.com>
2019-09-04 12:17:53 +0100

    added lab 11

--- labs/lab_11_polymorphism/Program.cs
+++ labs/lab_11_polymorphism/Program.cs
@@ -6,7 +6,57 @@ namespace lab_11_polymorphism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parent = new Parent("Dad", 40);
+            var child = new Child("Me", 23);
+            Console.WriteLine($"{parent.Name}'s age is {parent.Age}. {child.Name}'s age is {child.Age}.");
+            Console.WriteLine(child.Age);
+            for (int i = 0; i < 10; i++)
+            {
+                parent.HaveABrithday();
+                child.HaveABrithday();
+                Console.WriteLine($"{parent.Name}'s age is {parent.Age}. {child.Name}'s age is {child.Age}.");
+            }
+
         }
+
+
+        class Parent
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public Parent(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public Parent()
+            {
+
+            }
+
+            public void SetAge(int age)
+            {
+                Age = age;
+            }
+
+            public virtual void HaveABrithday() { Age++; }
+
+        }
+        class Child : Parent
+        {
+
+            public Child(string name, int age = 0) : 
+                base(name, age)
+            {
+
+            }
+            public override void HaveABrithday() 
+            {
+                Age += 2; 
+            }
+         }
+
     }
 }

75d810983fdfbb86ffa7903b8a036d976d096e1d
Samuel Ribeiro <SRibeiro@spartaglobal.com>
2019-09-04 10:14:55 +0100

    Took away one of the lab 8's and added lab 9

---
+++ labs/lab_09_constructors/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace lab_09_constructors
+{
+    class Program
+    {
+        static void Main()
+        {
+            var merc01 = new Mercades("CHASSIS1234ID", "red", 2500);
+            var merc02 = new Mercades();
+        }
+    }
+
+    class Mercades
+    {
+        private string _engineChassisID;
+        public string Colour { get; set; }
+        public int Length{ get; set; }
+
+        public Mercades() { }
+        public Mercades(string engineChassisID, string colour, int  length)
+        {
+            this._engineChassisID = engineChassisID;
+            this.Colour = colour;
+            this.Length = length;
+        }
+    }
+
+    class AClass : Mercades
+    {
+        public AClass() 
+        {
+
+        }
+    }
+
+    class A104 : AClass
+    {
+
+    }
+
+}

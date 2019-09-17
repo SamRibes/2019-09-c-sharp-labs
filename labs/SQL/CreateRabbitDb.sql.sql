90cd6cb7801abe4fde905198c5c15969cadb0502
Samuel Caires Ribeiro <samuel.c.ribeiro@outlook.com>
2019-09-17 16:19:07 +0100

    Added 16th & 17th Sep work

--- labs/SQL/CreateRabbitDb.sql.sql
+++ labs/SQL/CreateRabbitDb.sql.sql
@@ -15,19 +15,10 @@
 --use RabbitDb
 --go
 
---CREATE TABLE Rabbits(
---	RabbitID INT NOT NULL IDENTITY PRIMARY KEY,
---	Age INT,
---	Name NVARCHAR(50) NULL
---);
+CREATE TABLE Rabbits(
+	RabbitID INT NOT NULL IDENTITY PRIMARY KEY,
+	Age INT,
+	Name NVARCHAR(50) NULL
+);
 
-
-update Rabbits set Age = 99 where RabbitID = 5;
-
-delete from rabbits;
-
-SET IDENTITY_INSERT rabbits on
-insert into rabbits (rabbitid, age, name) values (1,0,'rabbits01')
-SET IDENTITY_INSERT rabbits on
-
-select * from Rabbits;
+insert into rabbits (age, name) values (0,'rabbits01')
---
+++ labs/lab_29_export_to_office/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed;
+using Xceed.Words.NET;
+
+namespace lab_29_export_to_office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var fileName = @"RabbitReport.docx";
+            var desktopFileName = @"" + desktopPath + "//" + fileName;
+            var doc = DocX.Create(desktopFileName);
+            string timeTaken1 = "7.1234", timeTaken1000 = "8.234";
+            Console.WriteLine(desktopFileName);
+
+            
+            doc.InsertParagraph("Rabbit Report");
+            doc.InsertParagraph("Number of rabbits created = 1000");
+            doc.InsertParagraph($"Time taken with 1 read = {timeTaken1} seconds");
+            doc.InsertParagraph($"Time taken with 1000 reads = {timeTaken1000} seconds");
+            doc.Save();
+
+            Process.Start("WINWORD.EXE", desktopFileName);
+
+
+        }
+    }
+}

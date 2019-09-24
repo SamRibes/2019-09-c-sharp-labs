c6290622a6bbbabca8622e094a6d61c3211f4d50
Samuel Caires Ribeiro <samuel.c.ribeiro@outlook.com>
2019-09-24 16:44:11 +0100

    Added events labs

---
+++ labs/lab_32_events/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab_32_events
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+
+        public static event MyDelegate MyEvent;
+        static void Main(string[] args)
+        {
+            MyEvent += MethodA;     //Pointer to a method but dont call the method right now
+            MyEvent += MethodB;     //Callback
+            MyEvent += MethodC;
+
+            MyEvent();
+        }
+
+        static void MethodA() { Console.WriteLine("Method A"); }
+        static void MethodB() { Console.WriteLine("Method B"); }
+        static void MethodC() { Console.WriteLine("Method C"); }
+    }
+}
---
+++ labs/lab_33_oop_events/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lab_33_oop_events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Sam = new Child();
+
+            Sam.Name = "Sam";
+            for (int i = 0; i < 10; i++)
+            {
+                Sam.Grow();
+            }
+        }
+    }
+
+    class Child
+    {
+        public delegate void BirthdayDelegate(string TypeOfParty);
+
+        public event BirthdayDelegate HaveABirthday;
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Child()
+        {
+            this.Age = 0;
+            Console.WriteLine("Congrats! Beautiful baby :)");
+            HaveABirthday += Celebrate;
+        }
+
+        int Celebrate()
+        {
+            Age++;
+            Console.WriteLine($"Celebrating another brithday: Age is {this.Age}");
+            return Age;
+        }
+
+        public void Grow()
+        {
+            int ageNow = HaveABirthday();
+        }
+    }
+}

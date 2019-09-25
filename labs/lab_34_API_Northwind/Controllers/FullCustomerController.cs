bb163fef182f352cd74bbe934ff63952b8483568
Samuel Caires Ribeiro <samuel.c.ribeiro@outlook.com>
2019-09-25 15:48:16 +0100

    Added labs 34 and 35

---
+++ labs/lab_34_API_Northwind/Controllers/FullCustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace lab_34_API_Northwind.Controllers
+{
+    public class FullCustomerController : ApiController
+    {
+        public static List<Customer> customers;
+
+        public List<Customer> GetCustomerNames()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                customers = db.Customers.ToList();
+            }
+            return customers;
+        }
+    }
+}
